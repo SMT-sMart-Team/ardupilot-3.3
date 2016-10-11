9b0b9a70c155d351ec8f7d34488976c807e518bf
roger zhao <general_zyj@163.com>
2016-10-11 19:40:34 +0800

    disable hashlet default

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -232,7 +232,7 @@
 #define KEEP_ALIVE_TIME_OUT_PRU 75 // 75*20ms
 
 // add by Mi for test SMT new board atsha204a debug using hashlet @2016-06-12
-#define SMT_HASHLET_USE
+// #define SMT_HASHLET_USE
 // add by ZhaoYJ for test SMT new board @2016-05-09
 #define SMT_NEW_SENSORS_BOARD 
 
