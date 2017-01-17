4bfecfd3ef065adb8e15394faac50537332e1bde
roger zhao <general_zyj@163.com>
2017-01-17 17:09:48 +0800

    disable RAW: compass & baro

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -248,10 +248,10 @@
 // #define SMT_CAPTURE_IMU_RAW
 //
 // add by ZhaoYJ@2016-12-10 for capturing raw data in baro 
-#define SMT_CAPTURE_BARO_RAW
+// #define SMT_CAPTURE_BARO_RAW
 //
 // add by ZhaoYJ@2016-12-10 for capturing raw data in compass 
-#define SMT_CAPTURE_COMPASS_RAW
+// #define SMT_CAPTURE_COMPASS_RAW
 
 // add by ZhaoYJ for new flight mode switch: CH5 (2 pos) + CH6 (3 pos)
 #define SMT_CH5_CH6_SWITCH
