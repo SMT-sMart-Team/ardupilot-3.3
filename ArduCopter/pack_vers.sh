fb14c19789c52b1aee4c41d074c577a0d7ae5bbb
roger zhao <general_zyj@163.com>
2017-03-31 21:04:12 +0800

    addtional tips: need strongly test with this version

--- ArduCopter/pack_vers.sh
+++ ArduCopter/pack_vers.sh
@@ -9,5 +9,6 @@ cp ArduCopter.elf APP-new/app_hexa
 make pxf-octa
 cp ArduCopter.elf APP-new/app_octa
 cp readme APP-new/
-tar -cf FW-v0.2.fc APP-new
-echo "package FW done\n========================================="
+export FW_NAME=FW-v0.2-`date +%Y%m%d`.fc
+tar -cf ${FW_NAME} APP-new
+echo "package FW ${FW_NAME} done\n========================================="
